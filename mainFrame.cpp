d849ec227d50924e7c10169a85459c63689bc78f
GreenHatCode <12gans3456@gmail.com>
2025-02-05 20:33:34 +0200

    Added source files for the solution

---
+++ mainFrame.cpp
@@ -0,0 +1,8 @@
+#include "mainFrame.h"
+
+
+mainFrame::mainFrame(const wxString& title)
+	:wxFrame(NULL, wxID_ANY, title)
+{
+
+}
---
+++ mainFrame.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "wx\wx.h"
+
+class mainFrame :
+    public wxFrame
+{
+public:
+    mainFrame(const wxString& title);
+
+private:
+
+};
+
