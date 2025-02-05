c1695d3a590eaa47f9aa501600710d18242291e6
GreenHatCode <12gans3456@gmail.com>
2025-02-05 21:10:08 +0200

    Added bare toolbar and todo-comments

--- mainFrame.h
+++ mainFrame.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "wx\wx.h"
 
-
-
-
-
+// toolbar images
+#include "res/cursor24.xpm"
+#include "res/delete.xpm"
+#include "res/node24.xpm"
+#include "res/edge24.xpm"
+#include "res/process24color.xpm"
+#include "res/help.xpm"
 
 
 class mainFrame :
