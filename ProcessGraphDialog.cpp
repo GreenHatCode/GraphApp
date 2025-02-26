4e583b908992deaee23e82347d508c30ed46dd7d
GreenHatCode <12gans3456@gmail.com>
2025-02-26 16:19:43 +0200

    Created a new empty class

---
+++ ProcessGraphDialog.cpp
@@ -0,0 +1,5 @@
+#include "ProcessGraphDialog.h"
+
+ProcessGraphDialog::ProcessGraphDialog(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style, const wxString& name)
+{
+}
