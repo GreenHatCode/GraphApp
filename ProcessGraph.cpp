2520fa02861bc32bf26f47672ae5fb0525bdab6d
GreenHatCode <12gans3456@gmail.com>
2025-03-01 03:51:21 +0200

    Added calculating early event data (t_early)

--- ProcessGraph.cpp
+++ ProcessGraph.cpp
@@ -10,6 +10,32 @@ bool ProcessGraph::DoProcess()
 {
 	if (m_graph_ptr->Empty())return false;
 
+	// module 1, DM lab4
+	// EARLY_EVENT_DATE
+	// algorithm
+	//
+	// 1) find all edges that incomes in the node
+	// 2) find the edge with max weight among these edges. This is our T_early
+	// If the node has'n any incoming edges, T_early = 0
+
+	// EARLY_EVENT_DATE
+	std::vector<int>T_early(m_graph_ptr->GetNodeAmount()); // result array
+	for (size_t i = 0; i < T_early.size(); i++)
+	{
+		int max_T_early = 0;
+		for (int k = i - 1; k >= 0; k--)
+		{
+			Edge* curr_incoming_edge = m_graph_ptr->GetEdge(m_graph_ptr->GetNode(k), m_graph_ptr->GetNode(i));
+			if (curr_incoming_edge == nullptr)continue;
+
+			int tmp = curr_incoming_edge->weight + T_early[k];
+			if (max_T_early < tmp) max_T_early = tmp;
+		}
+
+		T_early[i] = max_T_early;
+	}
+
+
 
 
 
