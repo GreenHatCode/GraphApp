326f188c810632d8de2bd89d8e35caea35c0092b
GreenHatCode <12gans3456@gmail.com>
2025-07-06 05:28:02 +0300

    Replaced array in matrix class on std::vector

--- src/ProcessGraph/Matrix/MatN.h
+++ src/ProcessGraph/Matrix/MatN.h
@@ -8,9 +8,9 @@ public:
     {
         m_dimensions = dimensions;
         m_total_elements = dimensions * dimensions;
-        m_matrix_array = std::make_unique<int[]>(m_total_elements); // fill matrix with 0
+        // fill matrix with 0
         for (size_t i = 0; i < m_total_elements; i++)
-            m_matrix_array[i] = 0;
+            m_matrix_array.push_back(0);
         
     }
 
@@ -41,5 +41,5 @@ public:
 private:
     int m_total_elements;
     int m_dimensions;
-    std::unique_ptr<int[]> m_matrix_array = nullptr;
+    std::vector<int> m_matrix_array;
 };
