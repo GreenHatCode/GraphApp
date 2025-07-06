3be0fe1d73060e6bdf288a522fad924491e8903c
GreenHatCode <12gans3456@gmail.com>
2025-07-06 06:13:50 +0300

    Rewrote class to use it as matrix wiht n rows and m columns

--- src/ProcessGraph/Matrix/MatN.h
+++ src/ProcessGraph/Matrix/MatN.h
@@ -1,34 +1,36 @@
 #pragma once
 #include "wx/wx.h"
 
-// represents a square matrix with N rows and cols
+// represents a matrix with N rows and M cols
 class MatN{
 public:
-    MatN(int dimensions)
+    MatN(int rows, int cols)
     {
-        m_dimensions = dimensions;
-        m_total_elements = dimensions * dimensions;
+        assert(rows >= 0);
+        assert(cols >= 0);
+        this->rows = rows;
+        this->cols = cols;
         // fill matrix with 0
-        for (size_t i = 0; i < m_total_elements; i++)
+        for (size_t i = 0; i < rows * cols; i++)
             m_matrix_array.push_back(0);
         
     }
 
     int& At(int row, int col) // get an element by indices
     {
-        assert(row >= 0 && row < m_dimensions);
-        assert(col >= 0 && col < m_dimensions);
-        return m_matrix_array[row * m_dimensions + col];
+        assert(row >= 0 && row < rows);
+        assert(col >= 0 && col < cols);
+        return m_matrix_array[row * cols + col];
     }
 
     const wxString toWxString() const
     {
         wxString matrix_str;
-        for (size_t i = 0; i < m_dimensions; i++)
+        for (size_t i = 0; i < rows; i++)
         {
-            for (size_t k = 0; k < m_dimensions; k++)
+            for (size_t k = 0; k < cols; k++)
             {
-                matrix_str.append(wxString::Format("%i ", m_matrix_array[i*m_dimensions + k]));
+                matrix_str.append(wxString::Format("%i ", m_matrix_array[i*cols + k]));
             }
             matrix_str.erase(matrix_str.end() - 1);
             matrix_str.append("\n");
@@ -40,6 +42,7 @@ public:
 
 private:
     int m_total_elements;
-    int m_dimensions;
+    int rows;
+    int cols;
     std::vector<int> m_matrix_array;
 };
